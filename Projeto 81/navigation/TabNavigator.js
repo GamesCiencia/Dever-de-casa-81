be1203a7fac31dbdea7480619b6292f22dc40891
GamesCiencia <150619201+GamesCiencia@users.noreply.github.com>
2024-02-13 13:23:46 -0300

    Add files via upload

---
+++ Projeto 81/navigation/TabNavigator.js
@@ -0,0 +1,29 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Feed from "../screens/Feed";
+
+const BottomTabNavigator = () => {
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    if (route.name == 'Feed'){
+                        iconName = focused
+                            ? 'book'
+                            : 'book-outline';
+                    } else if (route.name === 'createPost'){
+                        iconName = focused ? 'create' : 'create-outline';
+                    }
+                    return <Ionicons name={iconName} size={size} color={color} />
+                },
+            })}
+            tabBarOptions=({
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
+            })
+        >
+            <Tab.Screen name="Feed" component={Feed} />
+            <Tab.Screen name="CriarPost" component={CreatePost} />
+        </Tab.Navigator>     
+    );
+}
